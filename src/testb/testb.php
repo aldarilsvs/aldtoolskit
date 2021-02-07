ee1671c2137afbdce80a93165f6ce025b8699377
Aldaril <aldaril@gmail.com>
2021-02-07 19:35:57 +0200

    update

---
+++ src/testb/testb.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace aldarilsvs\testb;
+
+class testb
+{
+    public function __construct()
+    {
+        echo __CLASS__ . ' ';
+        echo __FILE__ . ':' . __LINE__ . PHP_EOL;
+    }
+}
