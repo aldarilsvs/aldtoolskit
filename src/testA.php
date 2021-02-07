7c4f9d25bc3485ea84ff5fb6a7fee9b26de590a4
Aldaril <aldaril@gmail.com>
2021-02-07 17:46:04 +0200

    update

--- src/testA.php
+++ src/testA.php
@@ -7,6 +7,6 @@ class testA
         if( __NAMESPACE__ )
             echo __NAMESPACE__ . '/';
         echo __CLASS__ . ' ' . PHP_EOL;
-        echo __DIR__ . '/' . __FILE__ . ':' . __LINE__;
+        echo __DIR__ . '/' . __FILE__ . ':' . __LINE__ . PHP_EOL;
     }
 }
