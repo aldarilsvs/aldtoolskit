5a0acf3a1ce7a9419b2e4ea078e1e0f9bfa707d5
Aldaril <aldaril@gmail.com>
2021-02-07 19:10:50 +0200

    update

--- src/testA.php
+++ src/testA.php
@@ -1,12 +1,12 @@
 <?php
 
+namespace aldarilsvs;
+
 class testA
 {
     public function __construct()
     {
-        if( __NAMESPACE__ )
-            echo __NAMESPACE__ . '/';
-        echo __CLASS__ . ' ' . PHP_EOL;
+        echo __CLASS__ . ' ';
         echo __DIR__ . '/' . __FILE__ . ':' . __LINE__ . PHP_EOL;
     }
 }
