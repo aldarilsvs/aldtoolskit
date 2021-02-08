223489a48f5c2a4c78c016bc81068fcf7ea7b7e9
Aldaril <aldaril@gmail.com>
2021-02-08 13:24:30 +0200

    2021-02-08 13:24:25 fix

---
+++ src/testc.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace aldarilsvs\aldtoolskit;
+
+/**
+ * Description of testc
+ *
+ * @author aldaril
+ */
+class testc
+{
+     public function __construct()
+     {
+         echo __FILE__ . ' : ' . __LINE__ . PHP_EOL;
+     }
+}
