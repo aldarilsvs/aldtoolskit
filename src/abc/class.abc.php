291f3ae67e985cd27b9069d4ac6a5600c204c26b
Aldaril <aldaril@gmail.com>
2021-02-08 08:37:15 +0200

    update 2021-02-08 08:37:13

---
+++ src/abc/class.abc.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace aldarilsvs\aldtoolskit;
+
+class abc {
+
+     public function __construct()
+     {
+         echo __FILE__ . ' : ' . __LINE__ . PHP_EOL;
+     }
+     
+     
+}
